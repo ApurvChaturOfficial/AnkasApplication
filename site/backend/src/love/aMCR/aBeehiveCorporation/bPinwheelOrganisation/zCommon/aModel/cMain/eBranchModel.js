899eee916111093d99f718c68d8cfabe38bd3a8a
ApurvChaturOfficial <apurvchaturoffiial@gmail.com>
2023-12-17 14:38:54 +0530

    Specifics Added

--- site/backend/src/love/aMCR/aBeehiveCorporation/bPinwheelOrganisation/zCommon/aModel/cMain/eBranchModel.js
+++ site/backend/src/love/aMCR/aBeehiveCorporation/bPinwheelOrganisation/zCommon/aModel/cMain/eBranchModel.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
 const timezone = require('moment-timezone');
-const defaultSchema = require('../../../../bFunction/jDefaultSchema');
+const defaultSchema = require('../../../../../../bFunction/jDefaultSchema');
 
 const schema = new mongoose.Schema({
   ...defaultSchema,
